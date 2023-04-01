bb39f5eb820d77112349f2ff43a0aa867ec0c4bd
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-01 06:17:22 -0300

    Iniciando atividade

---
+++ string/modulo_7.py
@@ -0,0 +1,16 @@
+"""
+Módulo 7 – Strings
+
+"""
+
+# 7.1.1 STRINGs no Python são listas:
+
+"""
+| 0 | 1 | 2 | 3 | 4 | 5 | 6 |  7  | 8 | 9  |10 |11 |12 |13 |14 |15 |16 |
+| g | a | b | r  | i  | e | l   | @ | g | m | a  | i  | l | . | c | o | m |
+"""
+
+# 7.2 Índice e tamanho da String
+email = 'gabriel@gmail.com'
+print(email)
+print(type(email))
