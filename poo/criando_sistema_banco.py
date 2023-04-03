caea3b5ea567c7778aa180512371a7583c67fc55
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-03 16:27:05 -0300

    Implementando o metodo para verificação do historico de transações bancarias

--- poo/criando_sistema_banco.py
+++ poo/criando_sistema_banco.py
@@ -3,11 +3,10 @@
 from datetime import datetime
 import pytz
 
-
 class ContaCorrente:
 
     def __init__(self, nome, cpf, numero_ag, numero_cc):
-        self.nome = nome
+        self._nome = nome
         self.cpf = cpf
         self.saldo = 0
         self.limite = None
@@ -22,7 +21,7 @@ class ContaCorrente:
         self.limite = -1000
         return self.limite
     def sacar(self, valor):
-        if self.saldo - valor < self.limite_conta():
+        if self.saldo - valor < self._limite_conta():
             print("Você não possui saldo suficiente !")
             self.consultar_saldo()
         else:
@@ -37,11 +36,11 @@ class ContaCorrente:
     def _data_hora():
         fuso_BR = pytz.timezone('Brazil/East')
         horario_BR = datetime.now(fuso_BR)
-        return horario_BR
-
+        return horario_BR.strftime('%d/%m/%Y  %H:%H:%S')
     def consultar_historico_transacoes(self, transacoes):
         print('Historico de transações')
         for transacoes in self.transacoes:
+            print(transacoes)
 
 
 
@@ -49,12 +48,16 @@ if __name__ == '__main__':
     # cliente = ContaCorrente()
     # print(cliente.nome) # None
     # print(cliente.cpf) # None
-    cliente = ContaCorrente('Gabriel', '453.923.225-49')
-    print(cliente.consultar_saldo())
-    cliente.depositar(343)
-    print(cliente.consultar_saldo())
-    cliente.sacar(934)
-    # print(cliente.consultar_saldo())
+    cliente_gabriel = ContaCorrente('Gabriel', '453.923.225-49', '1510', '1731-6')
+    print(cliente_gabriel .consultar_saldo())
+    cliente_gabriel .depositar(343)
+    print(cliente_gabriel .consultar_saldo())
+    cliente_gabriel .sacar(934)    # print(cliente.consultar_saldo())
+    cliente_gabriel .consultar_historico_transacoes(50)
+    cliente_afonso = ContaCorrente( 'Afonso', '711.961.805-23', '1510', '2242-8')
+    print(cliente_afonso.consultar_saldo())
+    print(cliente_afonso.nome)
+    print(cliente_afonso.agencia)
 
     # print(cliente.nome) # Gabriel
     # print(cliente.cpf) # 453.923.225-49
