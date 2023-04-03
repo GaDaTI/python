54319b1cc96964ec4b520365b8921ee60aa342b6
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-03 14:49:15 -0300

    Implementando dos metodo limite da conta

--- poo/criando_sistema_banco.py
+++ poo/criando_sistema_banco.py
@@ -1,18 +1,20 @@
 """ Criando um Sistema de Conta Corrente"""
 
-
 class ContaCorrente:
 
     def __init__(self, nome=None, cpf=None):
         self.nome = nome
         self.cpf = cpf
         self.saldo = 0
+        self.limite = None
     def consultar_saldo(self):
         texto = 'O seu saldo atual é de {}'.format(self.saldo)
         return texto
-
+    def limite_conta(self):
+        self.limite =  -1000
+        return self.limite
     def sacar(self, valor):
-        if self.saldo - valor < 0:
+        if self.saldo - valor < self.limite_conta( ):
             print("Você não possui saldo suficiente !")
             self.consultar_saldo()
         else:
@@ -29,10 +31,11 @@ if __name__ == '__main__':
     # print(cliente.cpf) # None
     cliente = ContaCorrente('Gabriel', '453.923.225-49')
     print(cliente.consultar_saldo())
-    cliente.depositar(50_000)
+    cliente.depositar(343)
     print(cliente.consultar_saldo())
-    cliente.sacar(12345)
+    cliente.sacar(934)
     print(cliente.consultar_saldo())
+   print(cliente.limite_conta)
     # print(cliente.nome) # Gabriel
     # print(cliente.cpf) # 453.923.225-49
 
