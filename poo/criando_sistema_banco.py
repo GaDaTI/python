8dde8cbfd78e60195c4f2a8dfb72afa779508206
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-03 16:55:50 -0300

    Implementando o metodo para realizar transferencia bancaria entre contas

--- poo/criando_sistema_banco.py
+++ poo/criando_sistema_banco.py
@@ -7,30 +7,31 @@ class ContaCorrente:
 
     def __init__(self, nome, cpf, numero_ag, numero_cc):
         self._nome = nome
-        self.cpf = cpf
-        self.saldo = 0
+        cpf1 = cpf
+        self.cpf = cpf1
+        self._saldo = 0
         self.limite = None
         self.agencia = numero_ag
         self.conta = numero_cc
         self.transacoes = [ ]
     def consultar_saldo(self):
-        texto = 'O seu saldo atual é de {}'.format(self.saldo)
+        texto = 'O seu saldo atual é de {}'.format(self._saldo)
         return texto
 
     def _limite_conta(self):
         self.limite = -1000
         return self.limite
     def sacar(self, valor):
-        if self.saldo - valor < self._limite_conta():
+        if self._saldo - valor < self._limite_conta():
             print("Você não possui saldo suficiente !")
             self.consultar_saldo()
         else:
-            self.saldo -= valor
-            self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
+            self._saldo -= valor
+            self.transacoes.append((valor, self._saldo, ContaCorrente._data_hora()))
         pass
     def depositar(self, valor):
-        self.saldo += valor
-        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
+        self._saldo += valor
+        self.transacoes.append((valor, self._saldo, ContaCorrente._data_hora()))
         pass
     @staticmethod
     def _data_hora():
@@ -42,6 +43,11 @@ class ContaCorrente:
         for transacoes in self.transacoes:
             print(transacoes)
 
+    def transferir(self, valor, conta_destino):
+        self._saldo -= valor
+        self.transacoes.append((-valor, self._saldo, ContaCorrente._data_hora()))
+        conta_destino._saldo += valor
+        conta_destino.transacoes.append((valor, conta_destino._saldo, ContaCorrente._data_hora()))
 
 
 if __name__ == '__main__':
@@ -56,8 +62,13 @@ if __name__ == '__main__':
     cliente_gabriel .consultar_historico_transacoes(50)
     cliente_afonso = ContaCorrente( 'Afonso', '711.961.805-23', '1510', '2242-8')
     print(cliente_afonso.consultar_saldo())
-    print(cliente_afonso.nome)
-    print(cliente_afonso.agencia)
+    cliente_gabriel.transferir(232,cliente_afonso)
+    print(cliente_afonso.consultar_saldo())
+    cliente_afonso.consultar_historico_transacoes(50)
+    print(cliente_gabriel.consultar_saldo())
+    #print(cliente_afonso.nome)
+    #print(cliente_afonso.agencia)
+
 
     # print(cliente.nome) # Gabriel
     # print(cliente.cpf) # 453.923.225-49
