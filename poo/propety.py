cf84033639e3dfcae320c03d142fda278d017c85
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-05 20:45:31 -0300

    Adicionando propety

--- poo/propety.py
+++ poo/propety.py
@@ -32,29 +32,43 @@ Para garantir a semantica correta  teriamos que fazer
 """  GETTER  E SETTER 
 # O getter retorna o valor de um atributo
 # O setter define um novo valor para um atributo
+
+# class Person:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.set_age(age)
+# 
+#     def set_age(self ,  age):
+#         if age <= 0:
+#             raise ValueError('The age must be positive')
+#         self._age = age
+# 
+#     def get_age(self):
+#         return self._age
+# 
+# 
+# if __name__ == '__main__' :
+#     pessoa = Person('Mugni', 25)
+#     print(pessoa.name)
+#     print(pessoa.get_age())
+#     #pessoa.set_age(-78)
+#     #print(pessoa.get_age())
+#     for metodo in dir(property()):
+#         print(metodo)
  """
+
 
 class Person:
     def __init__(self, name, age):
         self.name = name
         self.set_age(age)
-
-    def set_age(self ,  age):
-        if age <= 0:
-            raise ValueError('The age must be positive')
+    def set_age(self,  age):
+        if  age <= 0:
+            raise ValueError("The value must be positive")
         self._age = age
-
     def get_age(self):
-        return self._age
+        return  self._age
 
 
-if __name__ == '__main__' :
-    pessoa = Person('Mugni', 25)
-    print(pessoa.name)
-    print(pessoa.get_age())
-    #pessoa.set_age(-78)
-    #print(pessoa.get_age())
-    for metodo in dir(property()):
-        print(metodo)
 
 
