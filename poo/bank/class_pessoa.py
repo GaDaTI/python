415d69b554cdc5a0092b27b265b63d14ebab72fe
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-06 19:43:38 -0300

    Implementando funcionalidade rg e cpf

--- poo/bank/class_pessoa.py
+++ poo/bank/class_pessoa.py
@@ -3,49 +3,27 @@ from pprint import  pprint
 
 class Pessoa:
 
-    pais = {  'BR' : 'Brasil' }
+    nacionalidade = 'brasileiro'
 
-    def __init__(self, nome, sobrenome, idade, dia_de_nascimento, mes_de_nascimento, ano_de_nascimento, estado_civil, cpf, rg):
+    def __init__(self, nome, sobrenome, idade, dia_de_nascimento, mes_de_nascimento,
+                 ano_de_nascimento, estado_civil,  na ,  uf):
         """
         Dados de uma pessoa
         """
-        self.__nome = nome
+        self.nome = nome
         self.sobrenome = sobrenome
         self.idade = idade
         self.dia_de_nascimento = dia_de_nascimento
         self.mes_de_nascimento = mes_de_nascimento
         self.ano_de_nascimento = ano_de_nascimento
         self.estado_civil = estado_civil
-        self.cpf = cpf
-        self.rg = rg
-        print(f"Construindo o objeto ... {self}")
+        self.naturalidade = na
+        self.unidade_fiscal = uf
 
     def __str__(self):
-    nome = nome
-    sobrenome = sobrenome
-    idade = idade
-    dia_de_nascimento = dia_de_nascimento
-    mes_de_nascimento = mes_de_nascimento
-    ano_de_nascimento = ano_de_nascimento
-    estado_civil = estado_civil
-    cpf = cpf
-    rg = rg 
-
-
-
-
-
+        return f''' 
+'''
 if __name__ == '__main__' :
     pessoa = Pessoa()
     print(pessoa) # <__main__.Pessoa object at 0x7fab6f856a10>
     #pprint(pessoa.__dict__)
-    """
-    {'cpf': None,
- 'dia_de_nascimento': None,
- 'estado_civil': None,
- 'idade': None,
- 'mes_de_nascimento': None,
- 'nome': None,
- 'rg': None,
- 'sobrenome': None}
-    """
--- poo/bank/pessoa_brenda.py
+++ poo/bank/pessoa_brenda.py
@@ -2,6 +2,8 @@ from class_pessoa import Pessoa
 from pprint import  pprint
 
 if __name__ == '__main__' :
-    brenda = Pessoa( 'Brenda', 'Albuquerque',  '23',  '14',  '02' ,  '1983',  'solteira',  '777.192.323-21', '03010024-34' )
-    print(brenda._nome)
+    brenda =  Pessoa( 'Brenda', 'Albuquerque',  '23',  '14',  '02' ,  '1983',  'solteira', 'Salvador', 'BA')
+
+
+
 
