6e141fd9a30095ea00e820f6eacf05dcf219783d
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-08 07:27:09 -0300

    Definindo uma classe no python

---
+++ poo/fundamentos_poo/classe_e_objeto.py
@@ -0,0 +1,27 @@
+
+class ContaCorrente:
+    def mostar(self):
+        return  f"{self}"
+
+
+
+
+if __name__ == '__main__' :
+    """
+    class ContaCorrente:
+        def mostar(self):
+            return  f"{self}"
+            
+            
+    conta_corrente = ContaCorrente( )
+    print(conta_corrente)
+    print(conta_corrente.mostar()) 
+    <__main__.ContaCorrente object at 0x7f8c9335b7c0>
+    <__main__.ContaCorrente object at 0x7f8c9335b7c0>
+    
+    """
+    conta_corrente = ContaCorrente()
+    print(conta_corrente.mostar())
+
+
+
