0a2259a1e5b15beef1f0794fa1462c738b87560a
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-09 16:49:44 -0300

    Teste do getter e setter

--- poo/fundamentos_poo/test.py
+++ poo/fundamentos_poo/test.py
@@ -20,24 +20,4 @@
     """
 
 
-class Cliente:
 
-    def __init__(self, nome):
-        self.__nome = nome
-
-    @property
-    def g_nome(self):
-        return self.__nome
-
-
-    @nome.setter
-    def set_nome(self, outro_nome):
-        self.__nome = outro_nome
-        return self.__nome
-
-
-if __name__ == '__main__':
-    pessoa = Cliente("Augus")
-    print(pessoa.g_nome)
-    pessoa.nome("Outro")
-    print(pessoa.g_nome)
