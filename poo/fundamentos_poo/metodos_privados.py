dcf1c2b04e56778c2ba6b75c40a596da0ccfd74b
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-10 07:23:45 -0300

    criando os atributos de instancia

--- poo/fundamentos_poo/metodos_privados.py
+++ poo/fundamentos_poo/metodos_privados.py
@@ -1,20 +1,19 @@
 class Cliente:
 
-    def __init__(self, nome):
-        self.__nome = nome
+    def __init__(self, numero, saldo, titular, limite):
+        self.numero = numero
+        self.saldo = saldo
+        self.titular = titular
+        self.limite = limite
 
-    @property
-    def nome(self):
-        return self.__nome
+        
 
-    @nome.setter
-    def nome(self, novo_nome):
-        self.__nome = novo_nome
-        return self.__nome.upper()
+
+
 
 
 if __name__ == '__main__':
-    pessoa = Cliente('Vagner')
-    print(pessoa.nome)
-    pessoa.nome = 'Pedro'
-    print(pessoa.nome)
+    conta_tr = Cliente(1221)
+    print(conta_tr.saldo)
+    conta_tr.saldo = -20000
+
