dc200e83739bcbbe1af0cdf6ba63cb33c368d1f8
Gabriel Santana <prof.gabrielsantana@gmail.com>
2023-04-10 09:40:35 -0300

    Teste do metodo verificar

--- poo/fundamentos_poo/metodos_privados.py
+++ poo/fundamentos_poo/metodos_privados.py
@@ -2,18 +2,38 @@ class Cliente:
 
     def __init__(self, numero, saldo, titular, limite):
         self.numero = numero
-        self.saldo = saldo
+        self.__saldo = saldo
         self.titular = titular
-        self.limite = limite
+        self.__limite = limite
 
-        
+    @property
+    def saldo(self):
+        return self.__saldo
 
+    @saldo.setter
+    def saldo(self, valor):
+        self.__saldo += valor
 
+    @property
+    def limite(self):
+        self.__limite = self.__verificar()
+        return self.__limite
 
+    @limite.setter
+    def limite(self, valor):
+        if self.__verificar(valor) == True:
+            self.__limite -= 99
+            return self.__limite
+        return self.__limite
+
+    @property
+    def __verificar(self):
+        if valor < 10:
+            self.__limite += 17
+        return self.__limite
 
 
 if __name__ == '__main__':
-    conta_tr = Cliente(1221)
-    print(conta_tr.saldo)
-    conta_tr.saldo = -20000
-
+    anee = Cliente(5.921 - 8, 1000, 'Anee', 100)
+    print(anee.saldo)
+    print(anee.limite)
